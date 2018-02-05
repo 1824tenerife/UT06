611c9283741d3aae9f3fe658c6095978f17cc764
1824tenerife <1824tenerife>
2018-02-05 16:22:58 +0000

    Commit inicial

---
+++ src/com/cip/prog/UT7.java
@@ -0,0 +1,97 @@
+package com.cip.prog;
+
+import java.util.*;
+
+public class UT7 {
+
+	public static void main(String[] args) {
+
+		int[] numeros = new int[20];
+		for (int i = 0; i < numeros.length; i++)
+			numeros[i] = (int) (Math.random() * 10) + 1;
+
+		int opcion = 0;
+		int suma = 0;
+
+		Scanner teclado = new Scanner(System.in);
+
+		do {
+
+			System.out.println("Menu");
+			System.out.println("1.mayor ");
+			System.out.println("2.menor  ");
+			System.out.println("3.media ");
+			System.out.println("4.todos ");
+			System.out.println("0.Salir");
+			System.out.println("Introduzca una opción");
+
+		} while (opcion!= 0);
+
+	
+		int mayor = numeros[0];
+		for (int i = 1; i < numeros.length; i++) {
+			if (mayor < numeros[i]) {
+				mayor = numeros[i];
+			}
+		}
+		// System.out.println(mayor);
+		int menor = numeros[0];
+		for (int i = 1; i < numeros.length; i++) {
+			if (menor > numeros[i]) {
+				menor = numeros[i];
+			}
+		}
+		// System.out.println(menor);
+		int media = numeros[0];
+		for (int i = 1; i < numeros.length; i++) {
+
+			suma += numeros[i];
+
+			media = (suma / numeros.length);
+			// System.out.println(media);
+		
+		}
+		
+            int todos = numeros[0];
+			for (int i = 1 ; i < numeros.length; i++) {
+				
+			System.out.print(" " + numeros[i]);
+				
+				
+			}
+
+		
+
+			opcion = teclado.nextInt();
+
+			switch (opcion) {
+
+			case 1:
+				System.out.println(mayor);
+				break;
+			case 2:
+				System.out.println(menor);
+				break;
+			case 3:
+				System.out.println(media);
+				break;
+			case 4:
+				System.out.println(todos);
+				break;
+			case 0:
+				System.out.println("Adios");
+				break;
+			default:
+				System.out.println("Introduzca una opcion correcta, elija entre los numeros 0 y el 4 ");
+				break;
+
+			}while (opcion!=0);
+	}
+}
+		
+
+		// teclado.close();
+
+	
+
+
